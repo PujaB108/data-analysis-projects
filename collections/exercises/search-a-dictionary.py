d40425086612bfe093f745f3fe588a9930b02a06
Puja Bhutani <pujabhutani@Pujas-MacBook-Air.local>
2024-08-26 22:10:41 -0500

    PujaB exercises

--- collections/exercises/search-a-dictionary.py
+++ collections/exercises/search-a-dictionary.py
@@ -10,35 +10,35 @@ flavors = {
 }
 
 # Set a variable called choice to the flavor you want to search for.
+# The code below returns the output 2.25
 choice = 'saffron'
 
-# if choice in flavors:
-#     cost = flavors[choice]
-# else:
-#     cost = 0
+if choice in flavors:
+    cost = flavors[choice]
+else:
+    cost = 0
 
-# print(cost)
+print(cost)
 
-# # Use an if statement to check if choice is in the flavors dictionary.
-
-# if choice in flavors:
+# Use an if statement to check if choice is in the flavors dictionary.
+if choice in flavors:
     
-# # If it is, set another variable called cost to the value associated with choice.
+# If it is, set another variable called cost to the value associated with choice.
   
-#     cost = flavors[choice]
+    cost = flavors[choice]
 
-# # If it isn’t, set cost to 0.
+# If it isn’t, set cost to 0.
 
-# else:
-#     cost = 0
+else:
+    cost = 0
     
-# # Print the cost.
+# Print the cost.
 
-# print(cost)
+print(cost)
 
-### Search a Dictionary Part 2:
+# Search a Dictionary Part 2:
 
-## Initialize two variables: highest_cost to 0 and fanciest to an empty string.
+# Initialize two variables: highest_cost to 0 and fanciest to an empty string.
 
 highest_cost = 0
 fanciest = ""
@@ -50,14 +50,20 @@ for flavor in flavors:
 
 print('The fanciest flavor is', fanciest, 'and it costs', highest_cost)
 
-# ## Loop through the flavors dictionary using a for loop.
-# for flavor in flavors:
-# ## For each flavor, check if its price is higher than highest_cost.
+# Loop through the flavors dictionary using a for loop.
 
-#     if flavors[flavor] > highest_cost:
+for flavor in flavors:
+    
+# For each flavor, check if its price is higher than highest_cost.
+
+    if flavors[flavor] > highest_cost:
         
-# ## If it is, update fanciest to this flavor and highest_cost to its price.
-# fanciest = flavor
-# highest_cost = flavors[flavor]
-# ## After the loop, print the most expensive flavor.
-# print('The fanciest flavor is', fanciest, 'and it costs', highest_cost)
+# If it is, update fanciest to this flavor and highest_cost to its price.
+
+        fanciest = flavor
+        highest_cost = flavors[flavor]
+
+# After the loop, print the most expensive flavor.
+# The code below returns the output - The fanciest flavor is saffron and it costs 2.25
+
+print('The fanciest flavor is', fanciest, 'and it costs', highest_cost)
